f76aaec2d737b24cd729a6602e54e2fd218fc7ac
Yaseen Alali <108293462+0x43ow@users.noreply.github.com>
2023-11-10 13:43:54 +0200

    first commit

---
+++ Components/MediaElement.js
@@ -0,0 +1,12 @@
+const { PureComponent } = require("react");
+const { View, Text } = require("react-native");
+
+export class MediaElement extends PureComponent{
+    render(){
+        return(
+            <View>
+                <Text>Song1</Text>
+            </View>
+        )
+    }
+}
---
+++ Screens/ServerMedia.js
@@ -0,0 +1,12 @@
+const { PureComponent } = require("react");
+const { View, Text } = require("react-native");
+
+export class ServerMedia extends PureComponent{
+    render(){
+        return(
+            <View>
+                <Text>Welcome, Our lord and savior</Text>
+            </View>
+        )
+    }
+}
