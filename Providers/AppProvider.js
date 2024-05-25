57b95ff6690cd90b33e83b00cd7cf97be033ff16
Yaseen Alali <yaseenmo.alali@gmail.com>
2024-05-25 20:50:49 +0300

    Notification capabilities

--- Providers/AppProvider.js
+++ Providers/AppProvider.js
@@ -1,12 +1,28 @@
+import { useEffect } from 'react';
 import { AppContext } from '../Contexts/Contexts';
 import { useMedia } from '../hooks/useMedia';
 import { useMediaControls } from '../hooks/useMediaControls';
+import TrackPlayer, { AppKilledPlaybackBehavior, Capability } from 'react-native-track-player';
 
 
 const AppProvider = ({ children }) => {
   const media = useMedia();
   const mediaControls = useMediaControls();
 
+
+  useEffect(() => {
+    TrackPlayer.updateOptions({
+      android: {
+        appKilledPlaybackBehavior: AppKilledPlaybackBehavior.ContinuePlayback,
+      },
+      capabilities: [
+        Capability.Play,
+        Capability.Pause,
+        Capability.Stop,
+      ],
+      compactCapabilities: [Capability.Play, Capability.Pause],
+    });
+  }, []);
   return (
     <AppContext.Provider value={{ ...media, ...mediaControls }}>
       {children}
